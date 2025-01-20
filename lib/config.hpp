11cd0e8c36166b9335aafd5ccdb4c25fea9cf664
unknown <ml.jut@student.avans.nl>
2025-01-20 10:59:46 +0100

    setup project with vcpkg

---
+++ lib/config.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * \note Make a new file in this directory named config.hpp with a copy of the defines that are present here. 
+ */
+
+/**
+ * \name Download Directory Path
+ * \brief Path to download directory for web files
+ */
+#ifndef DOWNLOAD_DIR
+#define DOWNLOAD_DIR "C:\\Users\\Matthijs\\School\\cpp\\eindopdracht_cpp\\resources\\web"
+#endif
